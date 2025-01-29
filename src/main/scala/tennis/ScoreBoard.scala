b0f903ed2bed6eafad6eb7fa1f8f23855bd81890
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2025-01-29 17:35:59 +0100

    Revert "try to put again the score inside the Standard Phase"
    This reverts commit ff96bab141d59cce8d48c4c8ce2b09e22f5348a3.

--- src/main/scala/tennis/ScoreBoard.scala
+++ src/main/scala/tennis/ScoreBoard.scala
@@ -4,27 +4,33 @@ import tennis.Players._
 
 
 case class ScoreBoard() {
-  private var _phase: Phases = Standard(Love, Love)
+  private var _scores: (Score, Score) = (Love, Love)
+  private var _phase: Phases = Standard
 
   def phase: Phases = _phase
+  def scores: (Score, Score) = _scores
 
   def point(player: Player): Unit = {
     _phase = _phase match {
       case Advantage(name) if name == player.name => Game(player.name)
       case Advantage(_) => Deuce
       case Deuce => Advantage(player.name)
-      case Standard(score1, score2) => player match {
-        case Player1(name) => score1 match {
-          case Forty => Game(name)
-          case Thirty if score2 == Forty => Deuce
-          case _ => Standard(score1.next, score2)
+      case Standard => player match {
+        case Player1(name) => _scores match {
+          case (Forty, _) => Game(name)
+          case (Thirty, Forty) => Deuce
+          case _ => Standard
         }
-        case Player2(name) => score2 match {
-          case Forty => Game(name)
-          case Thirty if score1 == Forty => Deuce
-          case _ => Standard(score1, score2.next)
+        case Player2(name) => _scores match {
+          case (_, Forty) => Game(name)
+          case (Forty, Thirty) => Deuce
+          case _ => Standard
         }
       }
+    }
+    _scores = player match {
+      case Player1(_) => (_scores._1.next, _scores._2)
+      case Player2(_) => (_scores._1, _scores._2.next)
     }
   }
 }
